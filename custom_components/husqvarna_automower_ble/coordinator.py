f0bfd610c2b9f4fed0fd13778f9a325a4db6f44f
Andy Brown <andy@broadcast-tech.co.uk>
2024-09-24 22:59:58 +0100

    Fix indent

--- custom_components/husqvarna_automower_ble/coordinator.py
+++ custom_components/husqvarna_automower_ble/coordinator.py
@@ -130,11 +130,11 @@ class HusqvarnaCoordinator(DataUpdateCoordinator[dict[str, bytes]]):
                 data["statistics"] = await self.mower.command("GetAllStatistics")
                 _LOGGER.debug("statuses: " + str(data["statistics"]))
             except ValueError as e:
-            if "Data length mismatch" in str(e):
-                _LOGGER.debug("Known fail on GetAllStatistics - skipping")
-                data["statistics"] = None
-            else:
-                raise  # Re-raise the exception if it's not the known ValueError
+                if "Data length mismatch" in str(e):
+                    _LOGGER.debug("Known fail on GetAllStatistics - skipping")
+                    data["statistics"] = None
+                else:
+                    raise  # Re-raise the exception if it's not the known ValueError
 
             data["operatorstate"] = await self.mower.command("IsOperatorLoggedIn")
             _LOGGER.debug("IsOperatorLoggedIn: " + str(data["operatorstate"]))
